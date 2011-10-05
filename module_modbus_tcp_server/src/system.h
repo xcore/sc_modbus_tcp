278ccc3204cdb77082576cc760c98b91b9b7fc72
Vinith Mundhra <vinith@xmos.com>
2011-10-05 11:03:56 +0530

    Trial commit - do not use

---
+++ module_modbus_tcp_server/src/system.h
@@ -0,0 +1,40 @@
+/*==============================================================================
+Filename:
+Project :
+Author  :
+Version :
+Purpose
+--------------------------------------------------------------------------------
+
+Version History
+--------------------------------------------------------------------------------
+
+License
+--------------------------------------------------------------------------------
+The copyrights, all other intellectual and industrial property rights are
+retained by XMOS and/or its licensors.
+Terms and conditions covering the use of this code can be found in the Xmos
+End User License Agreement.
+
+Copyright XMOS Ltd 2011
+
+In the case where this code is a modification of existing code under a
+separate license, the separate license terms are shown below. The
+modifications to the code are still covered by the copyright notice above.
+==============================================================================*/
+
+/*------------------------------------------------------------------------------
+Include files
+------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+Typedefs
+------------------------------------------------------------------------------*/
+typedef unsigned char   uint8_t;
+typedef signed char     int8_t;
+typedef unsigned short  uint16_t;
+typedef signed short    int16_t;
+typedef unsigned int    uint32_t;
+typedef signed int      int32_t;
+
+/*============================================================================*/
